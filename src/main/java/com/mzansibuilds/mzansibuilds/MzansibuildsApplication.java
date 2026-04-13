974b5fba065d89c9819689a75064f65655b3f6de
Akhona <92857052+Koanah@users.noreply.github.com>
2026-04-13 23:45:01 +0200

    Initial commit for new setup

---
+++ src/main/java/com/mzansibuilds/mzansibuilds/MzansibuildsApplication.java
@@ -0,0 +1,13 @@
+package com.mzansibuilds.mzansibuilds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzansibuildsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzansibuildsApplication.class, args);
+	}
+
+}
