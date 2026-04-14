2b8d1c0e47b455d3f5603460938c80b063a98a9f
Akhona <92857052+Koanah@users.noreply.github.com>
2026-04-14 02:19:55 +0200

    updated application properties

--- src/main/java/com/mzansibuilds/MzansibuildsApplication.java
+++ src/main/java/com/mzansibuilds/MzansibuildsApplication.java
@@ -1,5 +1,4 @@
-package com.mzansibuilds.mzansibuilds;
-
+package com.mzansibuilds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
